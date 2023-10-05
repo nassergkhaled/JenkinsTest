82813f6da5499cde28e46723baea66533978bf9a
Nasser Al-den Ghassan Shareef Khaled <nassergk1@gmail.com>
2023-10-05 05:35:36 +0300

    Author Update

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -1,5 +1,6 @@
 package com.javaexpress.docker.dockerspringboothello;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,10 @@ public class DockerHelloWorld {
 	public String getName() {
 		
 		return "Welcome to deployment in docker";
+	}
+	@GetMapping(path = "/author")
+	public String authorName()
+	{
+		return "Nasser G Khaled";
 	}
 }
